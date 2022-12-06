e488763c98f4617745ff3fc8a8cb0e50770efccf
alpakas96 <jhurleyfilm@gmail.com>
2022-12-06 16:49:49 -0500

    delete buttons refresh entire page.

--- app.js
+++ app.js
@@ -1,45 +1,48 @@
 let container = document.querySelector(".newShot")
 let btn = document.querySelector(".newShotButton")
 let shotListArray = []; //holds all of the shot objects 
+let y = 0; 
 
 class shotObject {
     constructor (number) {
         this.name = ("shot" + number);
         this.htmlContent =`
-            <form class="shot${number}"
+            <form class="shot${number}" id="shot${number}">
                 <fieldset>
                     <legend>Shot ${number}:</legend>
                     <label for="fname">Enter Shot Description:</label><br>
                     <textarea id="fname" name="fname"> </textarea><br>
-                    <input type="submit" value="Generate">
-                    <input type="submit" value="Delete shot" class="shot${number}DeleteButton">
+                    <input type="submit" value="Make">
+                    <button class="shot${number}DeleteButton">Delete shot</button>
                     <script> 
-                        shot${number}DeleteButton.addEventListener (
-                            "click", 
-                            shot${number}.remove();
-                        ); 
+
+                        let deleteShot${number} = document.getElementById('shot${number}');
+                        document.querySelector(".shot${number}DeleteButton").addEventListener ("click", (e) => {
+                            e.preventDefault()
+                            deleteShot${number}.parentNode.removeChild(deleteShot${number})
+                        }); 
                     </script> 
                 </fieldset>
             </form>
-        `  
+        ` 
     }
 }
 
-// function Title(text){
-//     this.element = document.createElement('h2');
-//     this.element.innerText = text;
-//   }
+// shot${number}DeleteButton.addEventListener ("click", deleteShot${number}.remove(), reRender());
 
 function createNewShot(){
-    let newShot = new shotObject;(shotListArray.length); 
+    let newShot = new shotObject (shotListArray.length + 1); 
     shotListArray.push(newShot); 
-    container.insertAdjacentHTML("beforeend", shotListArray[0].htmlContent); 
+    let arrayLength = shotListArray.length
+    container.insertAdjacentHTML("beforeend", shotListArray[arrayLength-1].htmlContent); 
 }
 
 btn.addEventListener("click", createNewShot);
 
 
 
+
 // put all created dom element into an array (use a class / objects)
 // loop through entire array with if to find container to delete 
 // use delete array.value 
+
